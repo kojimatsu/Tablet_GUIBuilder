fd97dd45360073fc26f51e01babf53077bbd6815
kojimatsu <ma14097@shibaura-it.ac.jp>
2015-11-10 16:08:09 +0900

    up

--- app/src/main/java/jp/ac/shibaura_it/se/sayo/tablet_guibuilder/screen_edit/GestureDialog.java
+++ app/src/main/java/jp/ac/shibaura_it/se/sayo/tablet_guibuilder/screen_edit/GestureDialog.java
@@ -9,11 +9,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import jp.ac.shibaura_it.se.sayo.tablet_guibuilder.Debug;
 import jp.ac.shibaura_it.se.sayo.tablet_guibuilder.R;
 
 /**
@@ -21,8 +24,7 @@ import jp.ac.shibaura_it.se.sayo.tablet_guibuilder.R;
  */
 public class GestureDialog extends DialogFragment {
 
-
-    private ArrayList<String> itemlist = new ArrayList<String>();
+    private List<String> itemlist = new ArrayList<String>();
 
     public static GestureDialog newInstance(){
         return new GestureDialog();
@@ -31,30 +33,28 @@ public class GestureDialog extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view = inflater.inflate(R.layout.dialog_listview, null, false);
         // ダイアログ設定
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle("タイトル");
         builder.setNegativeButton("CLOSE", onCancelListener);
 
         // アダプタ設定
-        DialogListAdapter dialogListAdapter = new DialogListAdapter(getActivity());
-        itemlist.add("a");
-        dialogListAdapter.setList(itemlist);
-        ListView listView = (ListView) getActivity().findViewById(R.id.dialog_listView);
-        listView.setAdapter(dialogListAdapter);
+        itemlist.add("aAAA");
+        itemlist.add("bBBBB");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, itemlist);
+        ListView listView = new ListView(getActivity());
+        listView.setAdapter(arrayAdapter);
 
         // リストの行がタップされたとき
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView parent, View view, int position, long id) {
-
+                Debug.log("@@");
 
             }
         });
 
-        builder.setView(view);
+        builder.setView(listView);
         return builder.create();
     }
 
